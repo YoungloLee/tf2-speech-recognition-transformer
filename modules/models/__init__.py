f807cc77648fe8eef1e4a4e6cdead4f1cd31853b
YoungloLee <lyrzzang@gmail.com>
2020-06-29 14:53:46 +0900

    remove redundant files

--- modules/models/__init__.py
+++ modules/models/__init__.py
@@ -1,11 +1,5 @@
 def create_model(name, checkpoint_state, hparams):
-    if name == 'CTC':
-        from .ctc import SpeechNetwork
-        return SpeechNetwork(checkpoint_state, hparams)
-    elif name == 'LAS':
-        from .las_clova import SpeechNetwork
-        return SpeechNetwork(checkpoint_state, hparams)
-    elif name == 'Transformer':
+    if name == 'Transformer':
         from .transformer import SpeechNetwork
         return SpeechNetwork(checkpoint_state, hparams)
     else:
